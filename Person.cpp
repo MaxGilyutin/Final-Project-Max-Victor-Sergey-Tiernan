6d3b844a39d0595ae3edec87350765b0a7f2b9d6
MaxGilyutin <113302071+MaxGilyutin@users.noreply.github.com>
2022-11-09 09:06:32 -0500

    Add files via upload

---
+++ Person.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2022 University of Michigan EECS183
+ *
+ * Person.cpp
+ * Project UID 28eb18c2c1ce490aada441e65559efdd
+ *
+ * <#Names#>
+ * <#Uniqnames#>
+ *
+ * Final Project - Elevators
+ */
+
+#include "Person.h"
+#include <iostream>
+#include <cmath>
+#include <sstream>
+
+using namespace std;
+
+Person::Person(string inputString) : Person() {
+   
+}
+
+bool Person::tick(int currentTime) {
+
+    for (angerLevel = TICKS_PER_ANGER_INCREASE;
+         angerLevel < MAX_ANGER; angerLevel++){
+        if (angerLevel == MAX_ANGER){
+            return true;
+            }
+        
+        else if (angerLevel < MAX_ANGER){
+            return false;
+        }
+        
+    }
+    return false;
+}
+
+void Person::print(ostream &outs) {    
+    
+    cout << "f" << currentFloor << "t" << targetFloor << "a" << angerLevel;
+    
+}
+
+//////////////////////////////////////////////////////
+////// DO NOT MODIFY ANY CODE BENEATH THIS LINE //////
+//////////////////////////////////////////////////////
+
+Person::Person() {
+    turn = 0;
+    currentFloor = 0;
+    targetFloor = 0;
+    angerLevel = 0;
+}
+
+int Person::getTurn() const {
+    return turn;
+}
+
+int Person::getCurrentFloor() const {    
+    return currentFloor;
+}
+
+int Person::getTargetFloor() const {    
+    return targetFloor;
+}
+
+int Person::getAngerLevel() const {    
+    return angerLevel;
+}
+
+ostream& operator<< (ostream& outs, Person p)
+{
+    p.print(outs);
+    return outs;
+}
