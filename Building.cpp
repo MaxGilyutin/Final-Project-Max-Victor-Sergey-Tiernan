fda29fbff2ec8d034a82f440683fb59e13453607
skislenkov <113302912+skislenkov@users.noreply.github.com>
2022-11-21 15:17:07 -0700

    Update Building.cpp

--- Building.cpp
+++ Building.cpp
@@ -4,8 +4,8 @@
  * Building.cpp
  * Project UID 28eb18c2c1ce490aada441e65559efdd
  *
- * <#Names#>
- * <#Uniqnames#>
+ * Max Gilyutin, Victor Diaso, Tiernan Jesrani, Sergey Kislenkov
+ * mgily, vjdiaso, tiernanj, skis
  *
  * Final Project - Elevators
  */
