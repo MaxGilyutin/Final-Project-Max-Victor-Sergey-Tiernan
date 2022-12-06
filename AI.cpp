8d4ea3fbbbbb69d0db8e92390aade59bdee56362
Tiernan Jesrani <113256603+tiernanj@users.noreply.github.com>
2022-12-05 19:06:10 -0500

    Update AI.cpp

--- AI.cpp
+++ AI.cpp
@@ -17,6 +17,29 @@
 // You do not need to make any changes to this file for the Core
 
 string getAIMoveString(const BuildingState& buildingState) {
+    bool elevator0 = buildingState.elevators[0].isServicing;
+    bool elevator1 = buildingState.elevators[1].isServicing;
+    bool elevator2 = buildingState.elevators[2].isServicing;
+    int maxPriority;
+    int maxAngerLevel = 0;
+    for ( int i = 0; i < NUM_FLOORS; i++) {
+        for ( int j = 0; j < buildingState.floors[i].numPeople; i++) {
+            if (buildingState.floors[i].people[j].angerLevel > maxAngerLevel) {
+                maxPriority = i;
+            }
+        }
+    }
+    
+    /*if ( buildingState.elevators[0].isServicing != true) {
+        return "";
+    }
+    if (buildingState.elevators[1].isServicing != true) {
+        return "";
+    }
+    if ( buildingState.elevators[2].isServicing != true) {
+        return "";
+    }
+     */
     return "";
 }
 
